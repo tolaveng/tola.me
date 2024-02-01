4f53e27d13aac23ed99eaa1e7ad71811f2384326
tolaveng <tolaveng@hotmail.com>
2024-02-01 21:14:09 +1100

    fix parallax scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,43 @@ export default function HomePage() {
 
   return (
     <main className={styles.homeContainer}>
-      <div className={styles.headerContainer}></div>
-      <div className={styles.headerBackground}>
-      </div>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerBackground}>
+        </div>
 
-      <div className={styles.headerHero}>
-        <HeaderHero />
+        <div className={styles.headerHero}>
+          <HeaderHero />
+        </div>
+
+        <div className={`${styles.headerProfileSection} ${titleFont.className}`}>
+          <div className={styles.headerProfileImage}>
+            <img src="/images/profile.jpg" alt="Profile Image" style={{maxHeight: 200, maxWidth: 200}} />
+          </div>
+          <div className={styles.headerProfileTitle}>
+            Hi, I&apos;m Tola Veng
+          </div>
+
+          <div className={`${styles.headerProfileDescription}`}>
+            <div id="profileDescription" style={{minHeight: '2rem'}}>
+              <ul className="profileDescription" style={{display: 'none'}}>
+                <li data-in-effect="shake" data-out-effect="bounceOut">Welcome to my blog</li>
+                <li data-in-effect="flipInY" data-out-effect="fadeOut" data-in-shuffle="false" data-out-shuffle="false">I&apos;m a full-stack developer</li>
+                <li data-in-effect="rotateIn" data-out-effect="flipOutY" data-out-shuffle="true">I build Modern Web Mobile Applications</li>
+                <li data-in-effect="bounceIn" data-out-effect="rotateOutDownLeft" data-out-shuffle="true">.Net, C#, Blazor App, API, MVC </li>
+                <li data-in-effect="rotateInUpRight" data-out-effect="fadeOut" data-out-shuffle="true">ReactJs, React Native, Android & IOS</li>
+                <li data-in-effect="fadeIn" data-out-effect="bounceOut" data-out-shuffle="true">Azure, Aws & MongoAtlas</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.animateDownArrow} onClick={scrollToBody}>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
       </div>
 
       <div className={styles.headerIcons}>
@@ -38,36 +69,7 @@ export default function HomePage() {
         <Image src="/icons/swift.png" alt="" height="64" width="64"/>
       </div>
       
-      <div className={`${styles.headerProfileSection} ${titleFont.className}`}>
-        <div className={styles.headerProfileImage}>
-          <img src="/images/profile.jpg" alt="Profile Image" style={{maxHeight: 200, maxWidth: 200}} />
-        </div>
-        <div className={styles.headerProfileTitle}>
-          Hi, I&apos;m Tola Veng
-        </div>
-        <div className={`${styles.headerProfileDescription}`}>
-          <div id="profileDescription" style={{minHeight: '2rem'}}>
-            <ul className="profileDescription" style={{display: 'none'}}>
-              <li data-in-effect="shake" data-out-effect="bounceOut">Welcome to my blog</li>
-              <li data-in-effect="flipInY" data-out-effect="fadeOut" data-in-shuffle="false" data-out-shuffle="false">I&apos;m a full-stack developer</li>
-              <li data-in-effect="rotateIn" data-out-effect="flipOutY" data-out-shuffle="true">I build Modern Web Mobile Applications</li>
-              <li data-in-effect="bounceIn" data-out-effect="rotateOutDownLeft" data-out-shuffle="true">.Net, C#, Blazor App, API, MVC </li>
-              <li data-in-effect="rotateInUpRight" data-out-effect="fadeOut" data-out-shuffle="true">ReactJs, React Native, Android & IOS</li>
-              <li data-in-effect="fadeIn" data-out-effect="bounceOut" data-out-shuffle="true">Azure, Aws & MongoAtlas</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.animateDownArrow} onClick={scrollToBody}>
-        <div>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-      
-      <div ref={bodyRef} className={styles.blogPosts}>
+      <div ref={bodyRef} className={styles.bodySection}>
         <BlogPosts />
       </div>
     </main>
